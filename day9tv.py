44be5b5d2f441ce57e87d1ce7ad3e8e1ada1c7e8
Robert Drake <rfdrake+github@gmail.com>
2012-10-18 02:56:27 -0400

    Fixing bug where next page was taking you back to general archives instead of whatever specific search you were in

--- day9tv.py
+++ day9tv.py
@@ -75,7 +75,7 @@ class Day9tv:
             self.addCategory(title[i], 'http://day9.tv/'+url[i], 'showGames', '')
 
         page = int(get("page"))+1
-        url = 'http://day9.tv/archives/?page='+str(page)
+        url = get("url")+'?page='+str(page)
         self.addCategory('more episodes...', url, 'showTitles', page)
 
     def showGames(self, params = {}):
