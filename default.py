29da0cc6fc3a82416d8cfc8cbdf29187c88eb90e
Robert Drake <rfdrake+github@gmail.com>
2012-11-30 17:45:27 -0500

    Version number pulled from addon.xml instead of hardcoded in default.py implemented changes to addon.xml for Xbmc-addons pull (bump to version 2.0.0, fix xbmc.python version, add language)

--- default.py
+++ default.py
@@ -1,10 +1,10 @@
 import urllib, urllib2, re, sys, os, string, day9tv
 import xbmc, xbmcaddon, xbmcgui, xbmcplugin
 
-__version__ = "0.0.5b"
+__settings__ = xbmcaddon.Addon(id='plugin.video.day9tv')
+__version__ = __settings__.getAddonInfo('version')
 __plugin__ = "Day9tv-" + __version__
 __author__ = "Robert"
-__settings__ = xbmcaddon.Addon(id='plugin.video.day9tv')
 __language__ = __settings__.getLocalizedString
 
 Day9tv = day9tv.Day9tv()
